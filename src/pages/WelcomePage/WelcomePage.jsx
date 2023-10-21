1eeefe50de256d25300e06d3ad7ad76f475a1367
demonikos <119507967+demonikos@users.noreply.github.com>
2023-10-22 01:45:09 +0300

    item

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,10 +1,11 @@
+import { InfoItem } from 'components/InfoItem/InfoItem'
 import React from 'react'
 // import PropTypes from 'prop-types'
 
 const WelcomePage = props => {
   return (
     <div>
-      
+      <InfoItem value="2000"/>
     </div>
   )
 }
