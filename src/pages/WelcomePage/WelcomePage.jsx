81d3d000e78df15ed5b762454df1ba25bb369f04
Yulia-Queiroz <yuliabelkaone@gmail.com>
2023-10-22 13:24:58 +0300

    added adaptive layout

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,17 +1,19 @@
-import { InfoItem } from 'components/InfoItem/InfoItem'
-import React from 'react'
+import ExercisesSubcategoriesList from 'components/ExercisesSubcategoriesList/ExercisesSubcategoriesList';
+import { InfoItem } from 'components/InfoItem/InfoItem';
+import React from 'react';
 // import PropTypes from 'prop-types'
 
-const WelcomePage = props => {
+const WelcomePage = (props) => {
   return (
     <div>
-      <InfoItem name="Daily calorie intake" value="2000" variant="1"/>
+      <InfoItem name="Daily calorie intake" value="2000" variant="1" />
+      <ExercisesSubcategoriesList></ExercisesSubcategoriesList>
     </div>
-  )
-}
+  );
+};
 
 // WelcomePage.propTypes = {
 
 // }
 
-export default WelcomePage
+export default WelcomePage;
