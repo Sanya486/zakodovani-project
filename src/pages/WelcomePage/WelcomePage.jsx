143ab7bda83a623b004108dc6866f399f6114d1d
Yulia-Queiroz <yuliabelkaone@gmail.com>
2023-10-22 13:27:15 +0300

    Update WelcomePage.jsx

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,4 +1,3 @@
-import ExercisesSubcategoriesList from 'components/ExercisesSubcategoriesList/ExercisesSubcategoriesList';
 import { InfoItem } from 'components/InfoItem/InfoItem';
 import React from 'react';
 // import PropTypes from 'prop-types'
@@ -7,7 +6,6 @@ const WelcomePage = (props) => {
   return (
     <div>
       <InfoItem name="Daily calorie intake" value="2000" variant="1" />
-      <ExercisesSubcategoriesList></ExercisesSubcategoriesList>
     </div>
   );
 };
