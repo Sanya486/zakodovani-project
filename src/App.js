0dc3b77c5ab88f510f56838309852b0ead307a8e
Sanya486 <sanyok486@gmail.com>
2023-10-22 12:58:41 +0300

    change ' to "

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import './App.scss';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "./App.scss";
 
-import Layout from 'components/Layout/Layout';
-import WelcomePage from 'pages/WelcomePage/WelcomePage';
+import Layout from "components/Layout/Layout";
+import WelcomePage from "pages/WelcomePage/WelcomePage";
 // import RestrictedRoute from "components/Routes/RestrictedRoute";
-import SignUpPage from 'pages/SignUpPape/SignUpPage';
-import SignInPage from 'pages/SignInPage/SignInPage';
+import SignUpPage from "pages/SignUpPape/SignUpPage";
+import SignInPage from "pages/SignInPage/SignInPage";
 // import PrivateRoute from "components/Routes/PrivateRoute";
-import DairyPage from 'pages/DairyPage/DairyPage';
-import ProfilePage from 'pages/ProfilePage/ProfilePage';
-import ProductsPage from 'pages/ProductsPage/ProductsPage';
-import ExercisesPage from 'pages/ExercisesPage/ExercisesPage';
-import ErrorPage from 'pages/ErrorPage/ErrorPage';
+import DairyPage from "pages/DairyPage/DairyPage";
+import ProfilePage from "pages/ProfilePage/ProfilePage";
+import ProductsPage from "pages/ProductsPage/ProductsPage";
+import ExercisesPage from "pages/ExercisesPage/ExercisesPage";
+import ErrorPage from "pages/ErrorPage/ErrorPage";
 
 function App() {
   return (
