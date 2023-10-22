7e572aff002ae8504a082ba49846d5cb5709f26d
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-10-22 18:55:48 +0300

    убарал мусор

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,16 +2,14 @@ import Header from 'components/Header/Header';
 import React from 'react'
 import { Outlet } from "react-router-dom";
 // import PropTypes from 'prop-types'
-import Button from 'components/Button/Button';
 
 const Layout = props => {
   return (
     <>
       <Header />
-      <Outlet />
-      <Button title="Sign Up" />
+      <Outlet/>
     </>
-  );
+  )
 }
 
 // Layout.propTypes = {
