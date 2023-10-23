c0d62ac6428cf0c480e8128d187ebb16fa5e7b5e
Sanya486 <sanyok486@gmail.com>
2023-10-23 14:33:30 +0300

    clear Welcome

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,8 @@
-import BurgerMenu from 'components/BurgerMenu/BurgerMenu';
 import React from 'react';
 // import PropTypes from 'prop-types'
 
 const WelcomePage = () => {
-  return <div><BurgerMenu/></div>;
+  return <div></div>;
 };
 
 // WelcomePage.propTypes = {
