5b3558f5abe5d150d51fe08f98d55b4a9b684edb
Yulia-Queiroz <yuliabelkaone@gmail.com>
2023-10-23 17:17:43 +0300

    rendering by filter

--- src/components/ExercisesCategories/ExercisesCategories.jsx
+++ src/components/ExercisesCategories/ExercisesCategories.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import styles from './ExercisesCategories.module.scss';
 import clsx from 'clsx';
 
-const ExercisesCategories = () => {
-  const filters = useSelector((state) => state.sports.filter);
-
-  const uniqueFilterValues = [...new Set(filters.map((filter) => filter.filter))];
-  const [activeFilter, setActiveFilter] = useState(uniqueFilterValues[0]);
-
+const ExercisesCategories = ({ activeFilter, setActiveFilter, filterValues }) => {
   const handleFilterClick = (filterValue) => {
     setActiveFilter(filterValue);
   };
 
   return (
     <ul className={styles['category-list']}>
-      {uniqueFilterValues.map((filterValue) => (
+      {filterValues.map((filterValue) => (
         <li
           key={filterValue}
           onClick={() => handleFilterClick(filterValue)}
