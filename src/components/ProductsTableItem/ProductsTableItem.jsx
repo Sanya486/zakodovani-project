3d08de747d2cc0f2dbbbd817f40daec5e4a5b8b3
demonikos <119507967+demonikos@users.noreply.github.com>
2023-10-24 00:12:09 +0300

    changing styles

--- src/components/ProductsTableItem/ProductsTableItem.jsx
+++ src/components/ProductsTableItem/ProductsTableItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import css from './ProductsTableItem.module.scss';
 import sprite from '../../images/svg/sprite.svg';
-// import { clsx } from 'clsx';
+import { clsx } from 'clsx';
 
 export const ProductTableItem = ({ title, category, calories, weight, recommend }) => {
   return (
@@ -27,39 +27,39 @@ export const ProductTableItem = ({ title, category, calories, weight, recommend 
             <label htmlFor='title' className={css.title}>
               Title
             </label>
-            <Field id='title' name='title' className={css.field} />
+            <Field id='title' name='title' className={css.field} readOnly/>
           </div>
           <div className={css.fieldWrap}>
             <label htmlFor='category' className={css.title}>
               Category
             </label>
-            <Field id='category' name='category' className={css.field} />
+            <Field id='category' name='category' className={css.field} readOnly/>
           </div>
           <div className={css.blockWrap}>
             <div className={css.fieldWrap}>
               <label htmlFor='calories' className={css.title}>
                 Calories
               </label>
-              <Field id='calories' name='calories' className={css.field} />
+              <Field id='calories' name='calories' className={clsx(css.field, css.caloriesField)} readOnly/>
             </div>
             <div className={css.fieldWrap}>
               <label htmlFor='weight' className={css.title}>
                 Weight
               </label>
-              <Field id='weight' name='weight' className={css.field} />
+              <Field id='weight' name='weight' className={clsx(css.field, css.weightField)} readOnly/>
             </div>
             <div className={css.fieldWrap}>
               <label htmlFor='recommend' className={css.title}>
                 Recommend
               </label>
-              <Field id='recommend' name='recommend' className={css.field} />
+              <Field id='recommend' name='recommend' className={clsx(css.field, css.recommendField)} readOnly/>
             </div>
-            <button type='submit' className={css.button}>
+          </div>
+          <button type='submit' className={css.button}>
               <svg className={css.icon}>
                 <use href={sprite + '#trash_icon'}></use>
               </svg>
             </button>
-          </div>
         </Form>
       </Formik>
     </div>
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // import PropTypes from 'prop-types'
 
-import { InfoItem } from "components/InfoItem/InfoItem";
+// import { InfoItem } from "components/InfoItem/InfoItem";
 import { ProductTableItem } from 'components/ProductsTableItem/ProductsTableItem';
 
 const WelcomePage = () => {
   return <div>
-    <InfoItem name="Daily calorie intake" value="2000" variant="success"/>
+    {/* <InfoItem name="Daily calorie intake" value="2000" variant="success"/> */}
     <ProductTableItem 
     title="Bread Hercules grain" 
     category="Flour"
