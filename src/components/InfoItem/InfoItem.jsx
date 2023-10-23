3a94bd53b439516c9eb1ec25a48b10ec4e7dea18
nsihor <nsihor@gmail.com>
2023-10-23 21:16:14 +0300

    add DayDashboard

--- src/components/InfoItem/InfoItem.jsx
+++ src/components/InfoItem/InfoItem.jsx
@@ -22,16 +22,40 @@ export const InfoItem = ({ name, value, variant }) => {
       addClass = css.firstVariant;
   }
 
+  let addSvg = '';
+
+  switch (name) {
+    case 'Daily calorie intake':
+      addSvg = '#fluent_food_icon';
+      break;
+    case 'Daily norm of sports':
+      addSvg = '#dumbbell_icon';
+      break;
+    case 'Calories consumed':
+      addSvg = '#fluent_food_apple_icon';
+      break;
+    case 'Calories burned':
+      addSvg = '#fire_icon';
+      break;
+    case 'The rest of the calories':
+      addSvg = '#bubble_icon';
+      break;
+    case 'The rest of sports':
+      addSvg = '#running_stick_figure_icon';
+      break;
+    default:
+      addSvg = '#fluent_food_icon';
+  }
+
   return (
-    // <div className={css.infoItemContainer}>
-    <div className={clsx(css.infoItemContainer, addClass)}>
+    <li className={clsx(css.infoItemContainer, addClass)}>
       <div className={css.wrap}>
         <svg className={css.icon}>
-          <use href={sprite + '#fluent_food_icon'}></use>
+          <use href={sprite + addSvg}></use>
         </svg>
         <p className={css.itemName}>{name}</p>
       </div>
       <h2 className={css.itemValue}> {value}</h2>
-    </div>
+    </li>
   );
 };
