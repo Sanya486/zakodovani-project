2a1fb7dd36ab776240508883d0b85dc7c430a428
Sanya486 <sanyok486@gmail.com>
2023-10-24 20:52:08 +0300

    fix

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,11 +1,10 @@
-import ProductsFilters from 'components/ProductsFilters/ProductsFilters';
 import React from 'react';
 // import PropTypes from 'prop-types'
 
 const WelcomePage = () => {
   return (
     <div>
-      <ProductsFilters />
+    
 
     </div>
   );
