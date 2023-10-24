da09829c9417f9d1f023fa5cbde22ba41b59e2a6
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-10-24 12:07:53 +0200

    delete with Welcome

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,14 +1,12 @@
-// import ProductsFilters from 'components/ProductsFilters/ProductsFilters';
-import UserCard from 'components/UserCard/UserCard';
-
+import ProductsFilters from 'components/ProductsFilters/ProductsFilters';
 import React from 'react';
 // import PropTypes from 'prop-types'
 
 const WelcomePage = () => {
   return (
     <div>
-      {/* <ProductsFilters /> */}
-      <UserCard/>
+      <ProductsFilters />
+
     </div>
   );
 };
