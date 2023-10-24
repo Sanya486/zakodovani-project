c0c070100bf70f7c531923a88c4a0aec4c01c8d9
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-10-24 19:25:57 +0300

    135

--- src/components/ExercisesTableItem/ExercisesTableItem.jsx
+++ src/components/ExercisesTableItem/ExercisesTableItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import css from './ExercisesTableItem.module.scss';
 import { selectExercises } from 'redux/selectors';
 import { useSelector } from 'react-redux';
+import sprite from '../../images/svg/sprite.svg';
 
 const ExercisesTableItem = () => {
   const exercises = useSelector(selectExercises);
@@ -19,7 +20,6 @@ const ExercisesTableItem = () => {
   } else {
     return (
       <div>
-        
         {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
           <div className={css.bodybox} key={key}>
             <div className={css.container}>
@@ -39,21 +39,52 @@ const ExercisesTableItem = () => {
                 <h2 className={css.exercisename}>Name</h2>
                 <h3 className={css.exercisetipe}>{name}</h3>
               </div>
-              <div>
-                <h2 className={css.exercisename}>Target</h2>
-                <h3 className={css.exercisetipe}>{target}</h3>
-              </div>
-              <div>
-                <h2 className={css.exercisename}>Burned Calories</h2>
-                <h3 className={css.exercisetipe}>{burnedCalories}</h3>
-              </div>
-              <div>
-                <h2 className={css.exercisename}>Time</h2>
-                <h3 className={css.exercisetipe}>{time}</h3>
+              <div className={css.rowtext}>
+                <div>
+                  <h2 className={css.exercisename}>Target</h2>
+                  <h3 className={css.exercisetipe}>{target}</h3>
+                </div>
+                <div>
+                  <h2 className={css.exercisename}>Burned Calories</h2>
+                  <h3 className={css.exercisetipe}>{burnedCalories}</h3>
+                </div>
+                <div>
+                  <h2 className={css.exercisename}>Time</h2>
+                  <h3 className={css.exercisetipe}>{time}</h3>
+                </div>
+                <svg className={css.icon}>
+                  <use href={sprite + '#trash_icon'}></use>
+                </svg>
               </div>
             </div>
           </div>
         ))}
+        <table>
+          <thead className={css.exercisename}>
+            <tr>
+              <th>Body Part</th>
+              <th>Equipment</th>
+              <th>Name</th>
+              <th>Target</th>
+              <th>Burned Calories</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody className={css.exercisetipe}>
+            {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
+              <tr key={key}></tr>
+            <td>{bodyPart}</td>
+            <td>{equipment}</td>
+            <td>{name}</td>
+            <td>{target}</td>
+            <td>{burnedCalories}</td>
+            <td>{time}</td>
+              </tr>
+            ))}
+            
+          </tbody>
+        </table>
+        ;
       </div>
     );
   }
@@ -64,23 +95,3 @@ export default ExercisesTableItem;
 // import React from 'react';
 
 
-// <table>
-//   <thead className={css.exercisename}>
-//     <tr>
-//       <th>Body Part</th>
-//       <th>Equipment</th>
-//       <th>Name</th>
-//       <th>Target</th>
-//       <th>Burned Calories</th>
-//       <th>Time</th>
-//     </tr>
-//   </thead>
-//   <tbody className={css.exercisetipe}>
-//     <td>{bodyPart}</td>
-//     <td>{equipment}</td>
-//     <td>{name}</td>
-//     <td>{target}</td>
-//     <td>{burnedCalories}</td>
-//     <td>{time}</td>
-//   </tbody>
-// </table>;
