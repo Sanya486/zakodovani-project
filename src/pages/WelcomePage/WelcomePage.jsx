0a152b21d0335a19190984e3a5fab0de3075cc79
Oleksandr <120394126+Sanya486@users.noreply.github.com>
2023-10-25 16:03:00 +0300

    Update WelcomePage.jsx

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -5,24 +5,7 @@ import Subtext from 'components/Subtext/Subtext';
 // import { ProductTableItem } from 'components/ProductsTableItem/ProductsTableItem';
 
 const WelcomePage = () => {
-  return <div><Subtext page="signIn" />
-  
-  {/* <ProductTableItem 
-    title="Bread Hercules grain" 
-    category="Flour"
-    calories="289"
-    weight="100"
-    recommend="yes"
-    />
-    <ProductTableItem 
-    title="Rice semolina Garnets gluten-free Rice semolina Garnets gluten-free" 
-    category="Cereals"
-    calories="340"
-    weight="100"
-    recommend="no"
-    /> */}
-  
-  </div>;
+  return <div></div>;
 };
 
 // WelcomePage.propTypes = {
