5b5b80edc7597a654f95fa4926f66b31240992ac
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-10-25 18:14:33 +0300

    Check

--- src/components/ExercisesTable/ExercisesTable.jsx
+++ src/components/ExercisesTable/ExercisesTable.jsx
@@ -7,82 +7,79 @@ import clsx from 'clsx';
 
 const ExercisesTable = () => {
   const exercises = useSelector(selectExercises);
-  
 
-      /* mobile show */
-    return (
-      <div>
-        
-        <div className={clsx(css.bodybox)}>
-          {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
-            <div key={key}>
-              <div>
-                <h2 className={css.exercisename}>Body Part</h2>
-                <h3 className={css.exercisetipe}>{bodyPart}</h3>
+  /* mobile show */
+  return (
+    <div>
+      <div className={clsx(css.bodybox)}>
+        {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
+          <div key={key} className={css.itembox}>
+            <div>
+              <h2 className={css.exercisename}>Body Part</h2>
+              <h3 className={css.exercisetipe}>{bodyPart}</h3>
+            </div>
+            <div>
+              <h2 className={css.exercisename}>Equipment</h2>
+              <h3 className={css.exercisetipe}>{equipment}</h3>
+            </div>
+            <div>
+              <h2 className={css.exercisename}>Name</h2>
+              <h3 className={css.exercisetipe}>{name}</h3>
+            </div>
+            <div className={css.rowtext}>
+              <div className={css.rowtextitems}>
+                <h2 className={css.exercisename}>Target</h2>
+                <h3 className={css.exercisetipe}>{target}</h3>
               </div>
-              <div>
-                <h2 className={css.exercisename}>Equipment</h2>
-                <h3 className={css.exercisetipe}>{equipment}</h3>
+              <div className={css.rowtextitems}>
+                <h2 className={css.exercisename}>Burned Calories</h2>
+                <h3 className={css.exercisetipe}>{burnedCalories}</h3>
               </div>
-              <div>
-                <h2 className={css.exercisename}>Name</h2>
-                <h3 className={css.exercisetipe}>{name}</h3>
+              <div className={css.rowtextitems}>
+                <h2 className={css.exercisename}>Time</h2>
+                <h3 className={css.exercisetipe}>{time}</h3>
               </div>
-              <div className={css.rowtext}>
-                <div className={css.rowtextitems}>
-                  <h2 className={css.exercisename}>Target</h2>
-                  <h3 className={css.exercisetipe}>{target}</h3>
-                </div>
-                <div className={css.rowtextitems}>
-                  <h2 className={css.exercisename}>Burned Calories</h2>
-                  <h3 className={css.exercisetipe}>{burnedCalories}</h3>
-                </div>
-                <div className={css.rowtextitems}>
-                  <h2 className={css.exercisename}>Time</h2>
-                  <h3 className={css.exercisetipe}>{time}</h3>
-                </div>
-                <svg className={css.icon}>
+              <svg className={css.icon}>
+                <use href={sprite + '#trash_icon'}></use>
+              </svg>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* desk and tablet */}
+      <div className={clsx(css.bodyboxTable)}>
+        <table className={css.exercisestablehead}>
+          <thead>
+            <tr className={css.exercisename}>
+              <th>Body Part</th>
+              <th>Equipment</th>
+              <th>Name</th>
+              <th>Target</th>
+              <th>Burned Calories</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody className={css.exercisestablebody}>
+            {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
+              <tr key={key}>
+                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{bodyPart}</td>
+                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{equipment}</td>
+                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{name}</td>
+                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{target}</td>
+                <td className={clsx(css.exercisetipe, css.extrasmallwidth)}>{burnedCalories}</td>
+                <td className={css.exercisetipe}>{time}</td>
+                <svg className={css.icon2}>
                   <use href={sprite + '#trash_icon'}></use>
                 </svg>
-              </div>
-            </div>
-          ))}
-        </div>
-        {/* desk and tablet */}
-        <div className={clsx(css.bodyboxTable, css.ishidden)}>
-          <table className={css.exercisestablehead}>
-            <thead>
-              <tr className={css.exercisename}>
-                <th>Body Part</th>
-                <th>Equipment</th>
-                <th>Name</th>
-                <th>Target</th>
-                <th>Burned Calories</th>
-                <th>Time</th>
               </tr>
-            </thead>
-            <tbody>
-              {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
-                <tr key={key}>
-                  <td className={css.exercisetipe}>{bodyPart}</td>
-                  <td className={css.exercisetipe}>{equipment}</td>
-                  <td className={css.exercisetipe}>{name}</td>
-                  <td className={css.exercisetipe}>{target}</td>
-                  <td className={css.exercisetipe}>{burnedCalories}</td>
-                  <td className={css.exercisetipe}>{time}</td>
-                  <svg className={css.icon2}>
-                    <use href={sprite + '#trash_icon'}></use>
-                  </svg>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        ;
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-
+      ;
+    </div>
+  );
+};
 
 export default ExercisesTable;
 
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,15 @@ import Container from 'components/Container/Container';
 import Header from 'components/Header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import DayExercises from 'components/DayExercises/DayExercises';
 
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <Container />
+      <DayExercises/>
       <Outlet />
     </>
   );
