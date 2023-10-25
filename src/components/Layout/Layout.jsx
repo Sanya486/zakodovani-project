47de4fd85568ce885e2d0c2a5aed1d598414954a
Толочко Катерина <tolochko.kateryna@gmail.com>
2023-10-26 00:52:18 +0300

    Add ErrorPage

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+//import Container from 'components/Container/Container';
 import Header from 'components/Header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,67 @@
-import React from 'react';
-// import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react';
+import css from './ErrorPage.module.scss';
+import Button from 'components/Button/Button';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Logo from 'components/Logo/Logo';
 
 const ErrorPage = () => {
-  return <div></div>;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('/check-auth')
+      .then((response) => {
+        setIsAuthenticated(response.data.isAuthenticated);
+        setToken(response.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [isAuthenticated, token]);
+
+  const handleClick = () => {
+    const destination = isAuthenticated ? '/diary' : '/';
+    if (token) {
+      axios
+        .get(destination, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  return (
+    <div className={css.container}>
+      <Link to={isAuthenticated ? '/diary' : '/'} onClick={handleClick} className={css.logoWrapper}>
+        <Logo iconColor={'white-color'} />
+      </Link>
+      <div className={css.leftContainer}>
+        <h2 className={css.title}>404</h2>
+        <p className={css.text}>
+          Sorry, you have reached a page that we could not find. It seems that you are lost among
+          the numbers and letters of our virtual space. Perhaps this page went on vacation or
+          decided to disappear into another dimension. We apologize for this inconvenience.
+        </p>
+        <div>
+          <Button
+            title='Go Home'
+            styled='transparent'
+            classes={[css.button]}
+            onClick={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
-
-// ErrorPage.propTypes = {
-
-// }
 
 export default ErrorPage;
