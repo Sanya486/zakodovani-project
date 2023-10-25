0557587bab6ffe02f56f1bdd53130cbe8998c423
demonikos <119507967+demonikos@users.noreply.github.com>
2023-10-25 18:44:16 +0300

    modding TableItem

--- src/components/ProductsTableItem/ProductsTableItem.jsx
+++ src/components/ProductsTableItem/ProductsTableItem.jsx
@@ -49,103 +49,64 @@ export const ProductTableItem = ({ title, category, calories, weight, recommend,
 
   return (
     <>
-      {width > 767 && first !== 'true' ? (
-        <>
-          <div className={css.container}>
-            <div className={clsx(css.element, css.titleElement)}>
-              <p className={css.field}>{productTableItem.title}</p>
-            </div>
+        <div className={css.container}>
+          <div className={clsx(css.element, css.titleElement)}>
+            <p className={clsx(css.title, width > 767 && first !== 'true' && css.titleHidden)}>
+              Title
+            </p>
+            <p className={css.field}>{productTableItem.title}</p>
+          </div>
 
-            <div className={clsx(css.element, css.categoriesElement)}>
-              <p className={css.field}>{productTableItem.category}</p>
-            </div>
+          <div className={clsx(css.element, css.categoriesElement)}>
+            <p className={clsx(css.title, width > 767 && first !== 'true' && css.titleHidden)}>
+              Category
+            </p>
+            <p className={css.field}>{productTableItem.category}</p>
+          </div>
 
-            <div className={css.blockWrap}>
-              <div className={css.subBlockWrap}>
-                <div className={clsx(css.element, css.caloriesElement)}>
-                  <p className={css.field}>{productTableItem.calories}</p>
-                </div>
-
-                <div className={clsx(css.element, css.weightElement)}>
-                  <p className={css.field}>{productTableItem.weight}</p>
-                </div>
-
-                <div className={clsx(css.element, css.recommendElement)}>
-                  <p className={clsx(css.field, css.recommendField)}>
-                    <span
-                      className={clsx(
-                        css.recomendStatus,
-                        recommend !== 'yes' && css.recommendFalseColor,
-                      )}
-                    >
-                      {productTableItem.recommend}
-                    </span>
-                  </p>
-                </div>
+          <div className={css.blockWrap}>
+            <div className={css.subBlockWrap}>
+              <div className={clsx(css.element, css.caloriesElement)}>
+                <p className={clsx(css.title, width > 767 && first !== 'true' && css.titleHidden)}>
+                  Calories
+                </p>
+                <p className={css.field}>{productTableItem.calories}</p>
               </div>
 
-              <div className={css.buttonWrap}>
-                <button type='submit' className={css.button}>
-                  <svg className={css.icon}>
-                    <use href={sprite + '#trash_icon'}></use>
-                  </svg>
-                </button>
+              <div className={clsx(css.element, css.weightElement)}>
+                <p className={clsx(css.title, width > 767 && first !== 'true' && css.titleHidden)}>
+                  Weight
+                </p>
+                <p className={css.field}>{productTableItem.weight}</p>
               </div>
+
+              <div className={clsx(css.element, css.recommendElement)}>
+                <p className={clsx(css.title, width > 767 && first !== 'true' && css.titleHidden)}>
+                  Recommend
+                </p>
+                <p className={clsx(css.field, css.recommendField)}>
+                  <span
+                    className={clsx(
+                      css.recomendStatus,
+                      recommend !== 'yes' && css.recommendFalseColor,
+                    )}
+                  >
+                    {productTableItem.recommend}
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            <div className={css.buttonWrap}>
+              <button type='submit' className={css.button}>
+                <svg className={css.icon}>
+                  <use href={sprite + '#trash_icon'}></use>
+                </svg>
+              </button>
             </div>
           </div>
-        </>
-      ) : (
-        <>
-          <div className={css.container}>
-            <div className={clsx(css.element, css.titleElement)}>
-              <p className={css.title}>Title</p>
-              <p className={css.field}>{productTableItem.title}</p>
-            </div>
-
-            <div className={clsx(css.element, css.categoriesElement)}>
-              <p className={css.title}>Category</p>
-              <p className={css.field}>{productTableItem.category}</p>
-            </div>
-
-            <div className={css.blockWrap}>
-              <div className={css.subBlockWrap}>
-                <div className={clsx(css.element, css.caloriesElement)}>
-                  <p className={css.title}>Calories</p>
-                  <p className={css.field}>{productTableItem.calories}</p>
-                </div>
-
-                <div className={clsx(css.element, css.weightElement)}>
-                  <p className={css.title}>Weight</p>
-                  <p className={css.field}>{productTableItem.weight}</p>
-                </div>
-
-                <div className={clsx(css.element, css.recommendElement)}>
-                  <p className={css.title}>Recommend</p>
-                  <p className={clsx(css.field, css.recommendField)}>
-                    <span
-                      className={clsx(
-                        css.recomendStatus,
-                        recommend !== 'yes' && css.recommendFalseColor,
-                      )}
-                    >
-                      {productTableItem.recommend}
-                    </span>
-                  </p>
-                </div>
-              </div>
-
-              <div className={css.buttonWrap}>
-                <button type='submit' className={css.button}>
-                  <svg className={css.icon}>
-                    <use href={sprite + '#trash_icon'}></use>
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
-    </>
+        </div>
+      </>
   );
 };
 
