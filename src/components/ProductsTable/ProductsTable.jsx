04cd0f5aaf6e4923879b00a706bbe62c8e2bf011
demonikos <119507967+demonikos@users.noreply.github.com>
2023-10-25 16:40:18 +0300

    create new branch

---
+++ src/components/ProductsTable/ProductsTable.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import css from "./ProductsTable.module.scss"
+
+export const ProductTable = (obj) = {
+
+    
+
+    return (
+        <>
+        </>
+    )
+};
