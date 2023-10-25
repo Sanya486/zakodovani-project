78913cddf1b247eb85b46607c2fe46329652cc25
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-10-25 23:48:10 +0300

    try

--- src/components/ExercisesTable/ExercisesTable.jsx
+++ src/components/ExercisesTable/ExercisesTable.jsx
@@ -2,51 +2,32 @@ import React from 'react';
 import css from './ExercisesTable.module.scss';
 import { selectExercises } from 'redux/selectors';
 import { useSelector } from 'react-redux';
-import sprite from '../../images/svg/sprite.svg';
+import ExercisesTableItem from 'components/ExercisesTableItem/ExercisesTableItem';
 import clsx from 'clsx';
+
+
+
+// import React from 'react';
+
 
 const ExercisesTable = () => {
   const exercises = useSelector(selectExercises);
 
-  /* mobile show */
+
   return (
     <div>
       <div className={clsx(css.bodybox)}>
         {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
-          <div key={key} className={css.itembox}>
-            <div>
-              <h2 className={css.exercisename}>Body Part</h2>
-              <h3 className={css.exercisetipe}>{bodyPart}</h3>
-            </div>
-            <div>
-              <h2 className={css.exercisename}>Equipment</h2>
-              <h3 className={css.exercisetipe}>{equipment}</h3>
-            </div>
-            <div>
-              <h2 className={css.exercisename}>Name</h2>
-              <h3 className={css.exercisetipe}>{name}</h3>
-            </div>
-            <div className={css.rowtext}>
-              <div className={css.rowtextitems}>
-                <h2 className={css.exercisename}>Target</h2>
-                <h3 className={css.exercisetipe}>{target}</h3>
-              </div>
-              <div className={css.rowtextitems}>
-                <h2 className={css.exercisename}>Burned Calories</h2>
-                <h3 className={css.exercisetipe}>{burnedCalories}</h3>
-              </div>
-              <div className={css.rowtextitems}>
-                <h2 className={css.exercisename}>Time</h2>
-                <h3 className={css.exercisetipe}>{time}</h3>
-              </div>
-              <svg className={css.icon}>
-                <use href={sprite + '#trash_icon'}></use>
-              </svg>
-            </div>
-          </div>
-        ))}
+          <ExercisesTableItem
+            key={key}
+            bodyPart={bodyPart}
+            equipment={equipment}
+            name={name}
+            target={target}
+            burnedCalories={burnedCalories}
+            time={time}
+          />))}
       </div>
-      {/* desk and tablet */}
       <div className={clsx(css.bodyboxTable)}>
         <table className={css.exercisestablehead}>
           <thead>
@@ -61,26 +42,21 @@ const ExercisesTable = () => {
           </thead>
           <tbody className={css.exercisestablebody}>
             {exercises.map(({ key, bodyPart, equipment, name, target, burnedCalories, time }) => (
-              <tr key={key}>
-                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{bodyPart}</td>
-                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{equipment}</td>
-                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{name}</td>
-                <td className={clsx(css.exercisetipe, css.extrabigwidth)}>{target}</td>
-                <td className={clsx(css.exercisetipe, css.extrasmallwidth)}>{burnedCalories}</td>
-                <td className={css.exercisetipe}>{time}</td>
-                <svg className={css.icon2}>
-                  <use href={sprite + '#trash_icon'}></use>
-                </svg>
-              </tr>
+              <ExercisesTableItem
+            key={key}
+            bodyPart={bodyPart}
+            equipment={equipment}
+            name={name}
+            target={target}
+            burnedCalories={burnedCalories}
+            time={time}
+          />
             ))}
           </tbody>
         </table>
       </div>
-      ;
     </div>
   );
 };
 
 export default ExercisesTable;
-
-// import React from 'react';
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,20 @@
-import Container from 'components/Container/Container';
+// import Container from 'components/Container/Container';
 import Header from 'components/Header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import DayExercises from 'components/DayExercises/DayExercises';
+// import ExercisesTableItem from 'components/ExercisesTableItem/ExercisesTableItem';
+// import ExercisesTable from 'components/ExercisesTable/ExercisesTable';
 
 
 const Layout = () => {
+
+  
   return (
     <>
       <Header />
-      <Container />
-      
+      <DayExercises />
+
       <Outlet />
     </>
   );
