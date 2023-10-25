d562abb76b5a76547736d3e72bc07aada80bfb41
Oleksandr <120394126+Sanya486@users.noreply.github.com>
2023-10-25 11:53:38 +0300

    Update ExercisesTableItem.jsx

--- src/components/ExercisesTableItem/ExercisesTableItem.jsx
+++ src/components/ExercisesTableItem/ExercisesTableItem.jsx
@@ -8,8 +8,6 @@ import clsx from 'clsx';
 const ExercisesTableItem = () => {
   const exercises = useSelector(selectExercises);
 
-  console.log(exercises);
-
   if (exercises.length === 0) {
     return (
       <div className={css.bodybox}>
