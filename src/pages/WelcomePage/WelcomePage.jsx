6cee9e6f004210e03d85f85a58cd218476315e57
demonikos <119507967+demonikos@users.noreply.github.com>
2023-10-25 15:57:55 +0300

    nice try

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Subtext from 'components/Subtext/Subtext';
 // import PropTypes from 'prop-types'
 
-import { ProductTableItem } from 'components/ProductsTableItem/ProductsTableItem';
+// import { ProductTableItem } from 'components/ProductsTableItem/ProductsTableItem';
 
 const WelcomePage = () => {
   return <div><Subtext page="signIn" />
   
-  <ProductTableItem 
+  {/* <ProductTableItem 
     title="Bread Hercules grain" 
     category="Flour"
     calories="289"
@@ -20,7 +20,7 @@ const WelcomePage = () => {
     calories="340"
     weight="100"
     recommend="no"
-    />
+    /> */}
   
   </div>;
 };
