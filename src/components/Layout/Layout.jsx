b891d111adf8beb59f2a63ad55e409ca05b4b5a6
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-10-26 02:38:53 +0300

    finish

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,9 @@
-// import Container from 'components/Container/Container';
+import Container from 'components/Container/Container';
 import Header from 'components/Header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import DayExercises from 'components/DayExercises/DayExercises';
-// import ExercisesTableItem from 'components/ExercisesTableItem/ExercisesTableItem';
-// import ExercisesTable from 'components/ExercisesTable/ExercisesTable';
+
+
 
 
 const Layout = () => {
@@ -13,8 +12,7 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <DayExercises />
-
+      <Container />
       <Outlet />
     </>
   );
