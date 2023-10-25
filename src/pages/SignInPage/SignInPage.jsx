cdf7c14511706ba8d410f0519a92434962bdf234
DarynaTurash <daryna.turash@gmail.com>
2023-10-26 01:39:23 +0300

    test

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import SignInForm from 'components/SignInForm/SignInForm';
+import Subtext from 'components/Subtext/Subtext';
+import StatisticsInfo from 'components/StatisticsInfo/StatisticsInfo';
+import Container from 'components/Container/Container';
 // import PropTypes from 'prop-types'
 
 const SignInPage = () => {
-  return <div></div>;
+  return (
+          <>
+          <Container />
+          <SignInForm />
+          <Subtext page="SignInPage" />
+          <StatisticsInfo />
+          </>);
 };
 
 // SignInPage.propTypes = {
