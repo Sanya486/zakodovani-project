63d4f0da12cd8c495bfbb935d4e228c82eeeb6c0
Толочко Катерина <tolochko.kateryna@gmail.com>
2023-10-26 14:11:06 +0300

    Add ProductsPage

--- src/components/TitlePage/TitlePage.jsx
+++ src/components/TitlePage/TitlePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './TitlePage.module.scss';
+import clsx from 'clsx';
 
-const TitlePage = ({ children }) => {
-  return <h2 className={css.titlePage}>{children}</h2>;
+const TitlePage = ({ children, classes = [] }) => {
+  return <h2 className={clsx(css.titlePage, ...classes)}>{children}</h2>;
 };
 
 export default TitlePage;
+
+TitlePage.propTypes = {
+  classes: PropTypes.array,
+};
--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,8 +1,17 @@
+import TitlePage from 'components/TitlePage/TitlePage';
 import React from 'react';
 // import PropTypes from 'prop-types'
+import Container from 'components/Container/Container';
+import ProductsFilters from 'components/ProductsFilters/ProductsFilters';
+import css from './ProductsPage.module.scss';
 
 const ProductsPage = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <TitlePage classes={[css.title]}>Products</TitlePage>
+      <ProductsFilters />
+    </Container>
+  );
 };
 
 // ProductsPage.propTypes = {
