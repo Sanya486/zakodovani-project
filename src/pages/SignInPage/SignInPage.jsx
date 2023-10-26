3a3178c549ea644c9155332c5e18ff5531f59fa6
DarynaTurash <daryna.turash@gmail.com>
2023-10-26 15:13:14 +0300

    sighIn page

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import SignInForm from 'components/SignInForm/SignInForm';
 import Subtext from 'components/Subtext/Subtext';
 import StatisticsInfo from 'components/StatisticsInfo/StatisticsInfo';
-import Container from 'components/Container/Container';
+import TitlePage from 'components/TitlePage/TitlePage';
+import css from "./SignInPage.module.scss";
+import Text from "../../components/Text/Text";
 // import PropTypes from 'prop-types'
 
 const SignInPage = () => {
   return (
           <>
-          <Container />
-          <SignInForm />
-          <Subtext page="SignInPage" />
-          <StatisticsInfo />
+          <div className={css.background}>
+            <div className={css.containerWrapper}>
+              <div className={css.signUpBlockWrapper}>
+                <TitlePage>SignIn</TitlePage>
+                <Text>Welcome! Please enter your credentials to login to the platform:</Text>
+                <SignInForm />
+                <Subtext page="SignInPage" />
+              </div>
+              <StatisticsInfo />
+            </div>
+          </div>
           </>);
 };
 
