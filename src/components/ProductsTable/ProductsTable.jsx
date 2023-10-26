fd4e08a968f33366b73834ed2aa20ecd5149c69f
demonikos <119507967+demonikos@users.noreply.github.com>
2023-10-26 13:34:35 +0300

    last commit

--- src/components/ProductsTable/ProductsTable.jsx
+++ src/components/ProductsTable/ProductsTable.jsx
@@ -1,41 +1,27 @@
 import React from 'react';
-
-import css from "./ProductsTable.module.scss"
+import css from './ProductsTable.module.scss';
 import { ProductTableItem } from 'components/ProductsTableItem/ProductsTableItem';
-// import { useSelector } from "react-redux";
 
-// import { selectDiaryProducts } from "redux/selectors";
-// import { fetchDiaryDateInfo } from "redux/operations";
-// import { useEffect } from "react";
-
-export const ProductTable = ({products}) => {
-  // const dispatch = useDispatch();
-
-// const products = useSelector(selectDiaryProducts);
-
-  // useEffect(() => {
-  //     dispatch(fetchDiaryDateInfo());
-  // }, [dispatch]);
+export const ProductTable = ({ products }) => {
 
   console.log(products);
 
   return (
     <>
-      {/* {products.map(({ $oid: id, title, category, calories, weight, recommend }) => { */}
-
       <ul>
-      {products.map((product) => {
-        <li key={product._id.$oid} className={css.element}>
-          <ProductTableItem
-            title={product.title}
-            category={product.category}
-            calories={product.calories}
-            weight={product.weight}
-            recommend={product.recommend}
-            first={products.indexOf(product) === 0 ? "Yes" : "No"}
-          />
-        </li>;
-      })}
+        {products.map((product) => (
+          <li key={product._id.$oid} className={css.element}>
+            <ProductTableItem
+            id={product.id}
+              title={product.title}
+              category={product.category}
+              calories={product.calories}
+              weight={product.weight}
+              recommend={product.recommend}
+              first={products.indexOf(product) === 0 ? true : false}
+            />
+          </li>
+        ))}
       </ul>
     </>
   );
--- src/components/ProductsTableItem/ProductsTableItem.jsx
+++ src/components/ProductsTableItem/ProductsTableItem.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-// import css from './ProductsTableItem.module.scss';
 import css from './ProductsTableItem.module.scss';
 import sprite from '../../images/svg/sprite.svg';
 import { clsx } from 'clsx';
 
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchDeleteProduct } from 'redux/operations';
 
-export const ProductTableItem = ({ title, category, calories, weight, recommend, first }) => {
-  const productTableItem = {
-    title: title,
-    category: category,
-    calories: calories,
-    weight: weight,
-    recommend: recommend,
-    first: first,
+export const ProductTableItem = ({ id, title, category, calories, weight, recommend, first }) => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const dispatch = useDispatch();
+
+  const onDeleteProduct = (id) => {
+    dispatch(fetchDeleteProduct(id));
   };
 
-  const [width, setWidth] = useState(window.innerWidth);
+  const deleteHandler = () => {
+    onDeleteProduct(id);
+  };
 
   useEffect(() => {
     const handleResize = (event) => {
@@ -31,47 +31,33 @@ export const ProductTableItem = ({ title, category, calories, weight, recommend,
     };
   }, []);
 
-  //   const [state, setState] = useState({
-  //     title: '',
-  //     category: '',
-  //     calories: '',
-  //     weight: '',
-  //     recomend: '',
-  //     first: ''
-  //   });
-
-  // const dispatch = useDispatch();
-  // const products = useSelector(getDiaryProducts);
-
   return (
     <>
       <div className={css.container}>
         <div className={clsx(css.element, css.titleElement)}>
-          <p className={clsx(css.title, width > 767 && first !== 'true' && 'hidden')}>Title</p>
-          <p className={css.field}>{productTableItem.title}</p>
+          <p className={clsx(css.title, width > 767 && first !== true && 'hidden')}>Title</p>
+          <p className={css.field}>{title}</p>
         </div>
 
         <div className={clsx(css.element, css.categoriesElement)}>
-          <p className={clsx(css.title, width > 767 && first !== 'true' && 'hidden')}>Category</p>
-          <p className={css.field}>{productTableItem.category}</p>
+          <p className={clsx(css.title, width > 767 && first !== true && 'hidden')}>Category</p>
+          <p className={css.field}>{category}</p>
         </div>
 
         <div className={css.blockWrap}>
           <div className={css.subBlockWrap}>
             <div className={clsx(css.element, css.caloriesElement)}>
-              <p className={clsx(css.title, width > 767 && first !== 'true' && 'hidden')}>
-                Calories
-              </p>
-              <p className={css.field}>{productTableItem.calories}</p>
+              <p className={clsx(css.title, width > 767 && first !== true && 'hidden')}>Calories</p>
+              <p className={css.field}>{calories}</p>
             </div>
 
             <div className={clsx(css.element, css.weightElement)}>
-              <p className={clsx(css.title, width > 767 && first !== 'true' && 'hidden')}>Weight</p>
-              <p className={css.field}>{productTableItem.weight}</p>
+              <p className={clsx(css.title, width > 767 && first !== true && 'hidden')}>Weight</p>
+              <p className={css.field}>{weight}</p>
             </div>
 
             <div className={clsx(css.element, css.recommendElement)}>
-              <p className={clsx(css.title, width > 767 && first !== 'true' && 'hidden')}>
+              <p className={clsx(css.title, width > 767 && first !== true && 'hidden')}>
                 Recommend
               </p>
               <p className={clsx(css.field, css.recommendField)}>
@@ -81,14 +67,14 @@ export const ProductTableItem = ({ title, category, calories, weight, recommend,
                     recommend !== 'Yes' && css.recommendFalseColor,
                   )}
                 >
-                  {productTableItem.recommend}
+                  {recommend}
                 </span>
               </p>
             </div>
           </div>
 
           <div className={css.buttonWrap}>
-            <button type='submit' className={css.button}>
+            <button type='submit' className={css.button} onClick={deleteHandler}>
               <svg className={css.icon}>
                 <use href={sprite + '#trash_icon'}></use>
               </svg>
