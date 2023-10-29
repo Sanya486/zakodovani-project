906a4ebe405b5a51c423523b8fde7fb8ba2c71df
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-10-29 23:54:58 +0100

    add ParseInt

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { addYears, subYears } from 'date-fns';
-// import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import css from './UserForm.module.scss';
 import sprite from '../../images/svg/sprite.svg';
@@ -14,13 +13,14 @@ import { useDispatch } from 'react-redux';
 // import customWeekdayFormatter from './ustomWeekdayFormatter';
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().min(3).required("Це поле обов'язкове"),
-  email: Yup.string().email('Невірний формат Email'),
-  height: Yup.number().min(150, 'Мінімальна висота - 150 см').required("Це поле обов'язкове"),
-  currentWeight: Yup.number().min(35, 'Мінімальна вага - 35 кг').required("Це поле обов'язкове"),
-  desiredWeight: Yup.number().min(35, 'Мінімальна вага - 35 кг').required("Це поле обов'язкове"),
+  // name: Yup.string().min(3).required("Це поле обов'язкове"),
+  // email: Yup.string().email('Невірний формат Email'),
   birthday: Yup.date().required("Це поле обов'язкове"),
-  blood: Yup.string().required('Оберіть опцію Blood'),
+  blood: Yup.number().required('Оберіть опцію Blood'),
+   currentWeight: Yup.number().min(35, 'Мінімальна вага - 35 кг').required("Це поле обов'язкове"),
+  desiredWeight: Yup.number().min(35, 'Мінімальна вага - 35 кг').required("Це поле обов'язкове"),
+  height: Yup.number().min(150, 'Мінімальна висота - 150 см').required("Це поле обов'язкове"),
+  levelActivity: Yup.number().required('Оберіть опцію levelActivity'),
   sex: Yup.string().required('Оберіть стать'),
 });
 
@@ -36,9 +36,10 @@ const UserForm = () => {
     setCalendarIsClicked(false);
   };
 
-  const onClickDay = (value) => {
-    setCurrentDate(value);
-    closeCalendar(); // Закрити календар після вибору дати
+  const onClickDay = (date) => {
+    const isoDate = date.toISOString().split('T')[0];
+    setCurrentDate(isoDate);
+    closeCalendar();
   };
   const customWeekdayFormatter = (locale, date) => {
     const weekdays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
@@ -46,22 +47,22 @@ const UserForm = () => {
   };
 
   const handleSubmit = (values) => {
-    dispatch(fetchCalculateDailyMetrics);
+    values.blood = parseInt(values.blood);
+    values.levelActivity = parseInt(values.levelActivity);
+    dispatch(fetchCalculateDailyMetrics(values));
     console.log('Form submitted with values:', values);
   };
   // handleSubmit();
   return (
     <Formik
       initialValues={{
-        name: '',
-        email: '',
-        height: 0,
+        birthday: null,
+        blood: 0,
         currentWeight: 0,
         desiredWeight: 0,
-        birthday: currentDate.toISOString().split('T')[0],
-        blood: '1',
-        sex: 'Male',
-        levelActivity: '1',
+        height: 0,
+        levelActivity: 0,
+        sex: '',
       }}
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
@@ -72,22 +73,22 @@ const UserForm = () => {
             <span className={css.title}>Basic info</span>
             <div className={css.form}>
               <div className={css.tabletInput}>
-                <Field
+                {/* <Field
                   type='text'
                   id='name'
                   name='name'
                   className={`${css.inputBase} ${errors.name && touched.name ? css.error : ''}`}
                   required
-                />
-                <ErrorMessage name='name' component='div' className={css.errorName} />
+                /> */}
+                {/* <ErrorMessage name='name' component='div' className={css.errorName} /> */}
 
-                <Field
+                {/* <Field
                   type='email'
                   id='email'
                   name='email'
                   className={`${css.inputBase} ${errors.email && touched.email ? css.error : ''}`}
-                />
-                <ErrorMessage name='email' component='div' className={css.errorEmail} />
+                /> */}
+                {/* <ErrorMessage name='email' component='div' className={css.errorEmail} /> */}
               </div>
               <div className={css.groups}>
                 <div className={css.group1}>
@@ -149,18 +150,16 @@ const UserForm = () => {
                   </div>
                   <div className={css.column}>
                     <div className={css.forIcon}>
-                      <label  className={`${css.label} ${css.none}`}>
-                        Date:
-                      </label>
+                      <label className={`${css.label} ${css.none}`}>Date:</label>
                       <Field
                         type='date'
                         id='birthday'
                         name='birthday'
-                        onClick={showCalendar}
+                        // onClick={showCalendar}
                         className={`${css.input} ${css.inputDate} ${
                           errors.birthday && touched.birthday ? css.error : ''
                         }`}
-                        value={currentDate.toISOString().split('T')[0]}
+                        value={currentDate}
                         required
                       />
                       <svg onClick={showCalendar} className={css.calendarIcon}>
@@ -170,7 +169,7 @@ const UserForm = () => {
                         <Calendar
                           onChange={onClickDay}
                           value={currentDate}
-                          className={css.reactCalendar}
+                          // className={css.reactCalendar}
                           next2Label={null}
                           prev2Label={null}
                           locale='en'
@@ -194,43 +193,72 @@ const UserForm = () => {
           <p className={css.blood}>Blood</p>
           <div className={css.radio}>
             <div className={css.radioNumber}>
-              {[1, 2, 3, 4, 5].map((value) => (
-                <label key={value} className={`${css.labelMargin} ${css.customRadio}`}>
-                  <Field
-                    type='radio'
-                    name='blood'
-                    value={value.toString()}
-                    className={`${css.inputRadio} ${
-                      errors.blood && touched.blood ? css.error : ''
-                    }`}
-                  />
-                  <span>{value}</span>
-                </label>
-              ))}
-              <ErrorMessage name='blood' component='div' className={css.error} />
+              <label className={`${css.labelMargin} ${css.customRadio}`}>
+                <Field
+                  type='radio'
+                  name='blood'
+                  id="radio1"
+                  value="1"
+                  className={`${css.inputRadio} ${errors.blood && touched.blood ? css.error : ''}`}
+                />
+                <span>1</span>
+              </label>
+              <label className={`${css.labelMargin} ${css.customRadio}`}>
+                <Field
+                  type='radio'
+                  name='blood'
+                  value="2"
+                  id="radio2"
+                  className={`${css.inputRadio} ${errors.blood && touched.blood ? css.error : ''}`}
+                />
+                <span>2</span>
+              </label>
+              <label className={`${css.labelMargin} ${css.customRadio}`}>
+                <Field
+                  type='radio'
+                  name='blood'
+                  value="3"
+                  id="radio3"
+                  className={`${css.inputRadio} ${errors.blood && touched.blood ? css.error : ''}`}
+                />
+                <span>3</span>
+              </label>
+              <label className={`${css.labelMargin} ${css.customRadio}`}>
+                <Field
+                  type='radio'
+                  name='blood'
+                  id="radio4"
+                  value="4"
+                  className={`${css.inputRadio} ${errors.blood && touched.blood ? css.error : ''}`}
+                />
+                <span>4</span>
+              </label>
             </div>
+            <ErrorMessage name='blood' component='div' className={css.error} />
             <div className={css.sex}>
-              {['Male', 'Female'].map((option) => (
-                <label key={option} className={`${css.labelMargin} ${css.customRadio}`}>
-                  <Field
-                    type='radio'
-                    name='sex'
-                    value={option}
-                    className={`${css.inputRadioSex} ${errors.sex && touched.sex ? css.error : ''}`}
-                  />
-                  <span>{option}</span>
-                </label>
-              ))}
-              <ErrorMessage name='sex' component='div' className={css.error} />
-            </div>
+            {['male', 'female'].map((option) => (
+              <label key={option} className={`${css.labelMargin} ${css.customRadio}`}>
+                <Field
+                  type='radio'
+                  name='sex'
+                  value={option}
+                  className={`${css.inputRadioSex} ${errors.sex && touched.sex ? css.error : ''}`}
+                />
+                <span>{option}</span>
+              </label>
+            ))}
+            <ErrorMessage name='sex' component='div' className={css.error} />
           </div>
+          </div>
+          
+
           <div className={css.radioText}>
             <div className={css.groupsLAbel}>
               <label className={`${css.labelText} ${css.customRadio}`}>
                 <Field
                   type='radio'
                   name='levelActivity'
-                  value='1'
+                  value="1"
                   className={css.inputRadioText}
                   checked
                 />
@@ -241,7 +269,7 @@ const UserForm = () => {
             </div>
             <div className={css.groupsLAbel}>
               <label className={`${css.labelText} ${css.customRadio}`}>
-                <Field type='radio' name='levelActivity' value='2' className={css.inputRadioText} />
+                <Field type='radio' name='levelActivity' value="2" className={css.inputRadioText} />
                 <span className={css.spanName}>
                   Light activity (light exercises/sports 1-3 days per week)
                 </span>
@@ -249,7 +277,7 @@ const UserForm = () => {
             </div>
             <div className={css.groupsLAbel}>
               <label className={`${css.labelText} ${css.customRadio}`}>
-                <Field type='radio' name='levelActivity' value='3' className={css.inputRadioText} />
+                <Field type='radio' name='levelActivity' value="3" className={css.inputRadioText} />
                 <span className={css.spanName}>
                   Moderately active (moderate exercises/sports 3-5 days per week)
                 </span>
@@ -257,7 +285,7 @@ const UserForm = () => {
             </div>
             <div className={css.groupsLAbel}>
               <label className={`${css.labelText} ${css.customRadio}`}>
-                <Field type='radio' name='levelActivity' value='4' className={css.inputRadioText} />
+                <Field type='radio' name='levelActivity' value="4"className={css.inputRadioText} />
                 <span className={css.spanName}>
                   Very active (intense exercises/sports 6-7 days per week)
                 </span>
@@ -265,7 +293,7 @@ const UserForm = () => {
             </div>
             <div className={css.groupsLAbel}>
               <label className={`${css.labelText} ${css.customRadio}`}>
-                <Field type='radio' name='levelActivity' value='5' className={css.inputRadioText} />
+                <Field type='radio' name='levelActivity' value={5} className={css.inputRadioText} />
                 <span className={css.spanName}>
                   Extremely active (very strenuous exercises/sports and physical work)
                 </span>
