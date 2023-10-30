1502490a408d87501ff397ea804cb7853a3f4ec1
nsihor <nsihor@gmail.com>
2023-10-30 19:52:47 +0200

    add scroll bar

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,77 +1,8 @@
 import { ProductsItem } from 'components/ProductsItem/ProductsItem';
-import React, { useEffect } from 'react';
+import React from 'react';
 import css from './ProductsList.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectProducts } from 'redux/selectors';
-import { fetchProducts } from 'redux/operations';
 
-export const ProductsList = () => {
-  //   const exampleProductArr = [
-  //     {
-  //       isRecommend: true,
-  //       title: 'Rice semolina Garnets gluten-free',
-  //       calories: '340',
-  //       category: 'Cereals',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: false,
-  //       title: 'Venison . stew Special',
-  //       calories: '81',
-  //       category: 'Meat',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: true,
-  //       title: 'Rice semolina Garnets gluten-free',
-  //       calories: '300',
-  //       category: 'Cereals',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: true,
-  //       title: 'Rice semolina Garnets gluten-free',
-  //       calories: '300',
-  //       category: 'Cereals',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: true,
-  //       title: 'Rice semolina Garnets gluten-free',
-  //       calories: '340',
-  //       category: 'Cereals',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: false,
-  //       title: 'Venison . stew Special',
-  //       calories: '81',
-  //       category: 'Meat',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: true,
-  //       title: 'Rice semolina Garnets gluten-free',
-  //       calories: '340',
-  //       category: 'Cereals',
-  //       weight: '100',
-  //     },
-  //     {
-  //       isRecommend: false,
-  //       title: 'Venison . stew Special',
-  //       calories: '81',
-  //       category: 'Meat',
-  //       weight: '100',
-  //     },
-  //   ];
-
-  const dispatch = useDispatch();
-  const products = useSelector(selectProducts);
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
-
+export const ProductsList = ({ products }) => {
   return (
     <div className={css.productsListContainer}>
       <ul className={css.productList}>
