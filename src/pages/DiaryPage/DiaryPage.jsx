bdd0fd0e435a5e1e9e6d5670042c4cbd217de954
Sanya486 <sanyok486@gmail.com>
2023-11-01 04:49:40 +0200

    fix

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -13,7 +13,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import sprite from '../../images/svg/sprite.svg';
 import { DayProducts } from 'components/DayProdcuts/DayProducts';
-import formattingDate from 'utils/formattingDate';
+
+const formattingDate = (currentDate) => {
+  const day = currentDate.getDate().toString().padStart(2, '0');
+  const month = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+};
+
 
 const DiaryPage = () => {
   const dispatch = useDispatch();
