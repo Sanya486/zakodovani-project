1cba4e1d192b8e52d2d1c64338f7ab108bbce081
Sanya486 <sanyok486@gmail.com>
2023-11-03 17:05:53 +0200

    fix

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -20,7 +20,7 @@ const UserForm = () => {
     email: Yup.string().email('Invalid Email format'),
     birthday: Yup.string()
       .required('Required')
-      .matches(/^(0[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{4}$/,'Data must in dd/mm/yyyy format'),
+      .matches(/^(0[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{4}$/,'Data must be in dd/mm/yyyy format'),
     blood: Yup.number().required('Choose your blood'),
     currentWeight: Yup.number().min(35, 'Min weight - 35 kg').required('Required'),
     desiredWeight: Yup.number().min(35, 'Min weight - 35 kg').required('Required'),
