6bf37a57de27298f497bbc1b4b7a413cbb08ffb1
Mike Gusev <mihail.gusiev@gmail.com>
2024-06-29 22:01:15 +0300

    fix date issue

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -22,7 +22,7 @@ import { changeCurrentDate } from 'redux/diarySlice';
 
 const formattingDate = (currentDate) => {
   const day = currentDate.getDate().toString().padStart(2, '0');
-  const month = currentDate.getMonth() + 1;
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
   const year = currentDate.getFullYear();
   const formattedDate = `${year}-${month}-${day}`;
   return formattedDate;
